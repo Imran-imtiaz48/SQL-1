df83078a464ffba01c066f368005ebcbb7437622
ARMENEZ <simon.lamontagne88@gmail.com>
2020-01-30 14:32:11 +0100

    Ajout exercice 8

--- SQL.sql
+++ SQL.sql
@@ -5,3 +5,9 @@ use dataia_Nancy;
 SELECT anciennete,sinistre, count(*)
 FROM dataia_Nancy
 GROUP BY anciennete,sinistre ;
+
+/* Le nombre d'individu(comme prcedemment) en fonction du %age total*/ 
+SELECT anciennete,sinistre, count(*) /
+    (SELECT count(*) from dataia_Nancy)*100  /* Sous requete dans une fonction select, exprimé entre parenthese, multiplié par 100 pour avoir un %age*/ 
+FROM dataia_Nancy
+GROUP BY anciennete,sinistre ;
