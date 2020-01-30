03e432ba46136e97d31ccd6a4fe7d1a420aed3f8
ARMENEZ <simon.lamontagne88@gmail.com>
2020-01-30 14:26:31 +0100

    Ajout de l'exercice 7

--- SQL.sql
+++ SQL.sql
@@ -1,2 +1,7 @@
 # test
 use dataia_Nancy;
+
+/* Le nombre d'individus en fonction de l'anciennete et du sinistre*/ 
+SELECT anciennete,sinistre, count(*)
+FROM dataia_Nancy
+GROUP BY anciennete,sinistre ;
